9c722b6bbd2b03e711a32265c0e0b0f0c2f0c656
etper <piotr.brzezinski9@gmail.com>
2026-05-20 17:27:35 +0200

    Initial commit

---
+++ player.gd
@@ -0,0 +1,56 @@
+extends CharacterBody3D
+
+const SPEED = 6.0
+const JUMP_VELOCITY = 4.5
+const MOUSE_SENS = 0.002
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var camera = $Camera3D
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENS)
+		
+		camera.rotate_x(-event.relative.y * MOUSE_SENS)
+		
+		camera.rotation.x = clamp(
+			camera.rotation.x,
+			deg_to_rad(-90),
+			deg_to_rad(90)
+		)
+
+func _physics_process(delta):
+
+	# gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# jump
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# movement input
+	var input_dir = Input.get_vector(
+		"ui_left",
+		"ui_right",
+		"ui_up",
+		"ui_down"
+	)
+
+	var direction = (
+		transform.basis *
+		Vector3(input_dir.x, 0, input_dir.y)
+	).normalized()
+
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
